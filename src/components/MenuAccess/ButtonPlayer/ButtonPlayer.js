02ca445bd697bc1baf04ecb2a1c065b247f1087c
NguyenDat2904 <dathhcc2@gmail.com>
2024-08-28 00:37:45 +0700

    update music

--- src/components/MenuAccess/ButtonPlayer/ButtonPlayer.js
+++ src/components/MenuAccess/ButtonPlayer/ButtonPlayer.js
@@ -1,38 +1,90 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import './ButtonPlayer.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay, faPause, faForwardStep, faBackwardStep } from '@fortawesome/free-solid-svg-icons';
+import Song1 from '../../../assets/music/Ido.mp3';
+import Song2 from '../../../assets/music/beautifulinwhite.mp3';
+import Song3 from '../../../assets/music/talacuanhau.mp3';
+import Song4 from '../../../assets/music/xungdoicuoithoi.mp3';
+import Song5 from '../../../assets/music/yesido.mp3';
+import Song6 from '../../../assets/music/yeukhongcanhua.mp3';
+
 function ButtonPlayer() {
+   const songList = [Song1, Song2, Song3, Song4, Song5, Song6];
+   const [currentSongIndex, setCurrentSongIndex] = useState(Math.floor(Math.random() * songList.length));
+   const [isPlaying, setIsPlaying] = useState(true);
+
+   const audioRef = useRef(null);
+
+   useEffect(() => {
+      if (audioRef.current) {
+         audioRef.current.play().catch((error) => console.error('Error attempting to play audio:', error));
+      }
+   }, [currentSongIndex]);
+
+   useEffect(() => {
+      if (audioRef.current) {
+         audioRef.current.play().catch((error) => console.error('Error attempting to play audio:', error));
+      }
+   }, [audioRef.current]);
+
+   const togglePlayPause = () => {
+      if (isPlaying) {
+         audioRef.current.pause();
+      } else {
+         audioRef.current.play();
+      }
+      setIsPlaying(!isPlaying);
+   };
+
+   const nextSong = () => {
+      setCurrentSongIndex((prevIndex) => (prevIndex + 1) % songList.length);
+   };
+
+   const prevSong = () => {
+      setCurrentSongIndex((prevIndex) => (prevIndex - 1 + songList.length) % songList.length);
+   };
+
    return (
-      <div className="bii-player" style={{ display: 'block' }}>
-         <div className="bii-player-secondary">
-            <div className="bii-player-secondary-content">Click vào đây nếu bạn muốn phát nhạc!</div>
+      <div>
+         <audio ref={audioRef} src={songList[currentSongIndex]} autoPlay />
+
+         <div className="bii-player bii-prev" style={{ display: 'block' }} onClick={prevSong}>
+            <div className="bii-player-secondary">
+               <div className="bii-player-secondary-content">Bài trước</div>
+            </div>
+            <div className="playerIcon">
+               <span style={{ display: 'block' }} id="playerVolumeOn">
+                  <FontAwesomeIcon icon={faBackwardStep} />
+               </span>
+            </div>
          </div>
-         <div className="playerIcon">
-            <span id="playerVolumeOff" style={{ display: 'block' }}>
-               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={25}
-                  height={27}
-                  fill="#fff"
-                  className="bi bi-volume-mute-fill"
-                  viewBox="0 0 16 16"
-               >
-                  <path d="M6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06zm7.137 2.096a.5.5 0 0 1 0 .708L12.207 8l1.647 1.646a.5.5 0 0 1-.708.708L11.5 8.707l-1.646 1.647a.5.5 0 0 1-.708-.708L10.793 8 9.146 6.354a.5.5 0 1 1 .708-.708L11.5 7.293l1.646-1.647a.5.5 0 0 1 .708 0z" />
-               </svg>
-            </span>
-            <span style={{ display: 'none' }} id="playerVolumeOn">
-               <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width={25}
-                  height={27}
-                  fill="#fff"
-                  className="bi bi-volume-up-fill"
-                  viewBox="0 0 16 16"
-               >
-                  <path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z" />
-                  <path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z" />
-                  <path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z" />
-               </svg>
-            </span>
+
+         <div className="bii-player" style={{ display: 'block' }} onClick={togglePlayPause}>
+            <div className="bii-player-secondary">
+               <div className="bii-player-secondary-content">
+                  Click vào đây để {isPlaying ? 'tạm dừng' : 'phát nhạc'}!
+               </div>
+            </div>
+            <div className="playerIcon">
+               <span style={{ display: isPlaying ? 'none' : 'block' }} id="playerVolumeOff">
+                  <FontAwesomeIcon icon={faPlay} />
+               </span>
+               <span style={{ display: isPlaying ? 'block' : 'none' }} id="playerVolumeOn">
+                  <FontAwesomeIcon icon={faPause} />
+               </span>
+            </div>
+         </div>
+
+         <div className="bii-player bii-next" style={{ display: 'block' }} onClick={nextSong}>
+            <div className="bii-player-secondary">
+               <div className="bii-player-secondary-content">Bài tiếp theo</div>
+            </div>
+            <div className="playerIcon">
+               <span style={{ display: 'block' }} id="playerVolumeOn">
+                  <FontAwesomeIcon icon={faForwardStep} />
+               </span>
+            </div>
          </div>
       </div>
    );
